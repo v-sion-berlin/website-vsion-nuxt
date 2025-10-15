67ddbd4a1a0c914dda8da9cc6ffda04d633bf22d
lukasklier-v-sion <lukas.klier@v-sion.de>
2025-10-15 15:05:00 +0200

    feat: add ProjectsOverview

---
+++ app/components/content/ProjectsOverview.vue
@@ -0,0 +1,39 @@
+<template>
+  <section id="hero">
+    <div id="headline">
+      <h1>
+        <slot mdc-unwrap="p" />
+      </h1>
+      <div class="description">
+        <slot name="description" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+#hero {
+  position: relative;
+  overflow: hidden;
+  min-height: max-content;
+}
+
+#headline {
+  position: relative;
+  max-width: 100%;
+  padding: clamp(10rem, 12vw, 13.75rem) clamp(2rem, 10vw, 19.125rem)
+    clamp(2rem, 5vw, 4rem) clamp(0rem, 5vw, 5.625rem);
+}
+
+.description {
+  margin-top: 2rem;
+  color: #ffffff;
+  font-size: clamp(16px, 1.5vw, 32px);
+  font-family: Montserrat, sans-serif;
+  font-weight: 400;
+  max-width: 70%;
+  line-height: 1.6;
+}
+</style>
