358e4f18f29cb23824866caa27cf80d9e1cbea8c
lukasklier-v-sion <lukas.klier@v-sion.de>
2025-10-30 15:58:00 +0100

    feat: add career images section content component

---
+++ app/components/content/CareerImagesSection.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <main>
+      <section class="image-wrapper">
+        <div class="first-image">
+          <img :src="careerImage1.src" v-if="careerImage1" />
+        </div>
+        <div>
+          <img :src="careerImage2.src" v-if="careerImage2" />
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  careerImage1?: { src: string; alt: string };
+  careerImage2?: { src: string; alt: string };
+}>();
+</script>
+
+<style scoped>
+.image-wrapper {
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+  padding: clamp(2rem, 5vw, 4rem) 0 clamp(2rem, 5vw, 4rem)
+    clamp(1rem, 5vw, 5.625rem);
+  margin-top: clamp(-10rem, -15vw, -6rem);
+  height: 50vw;
+  position: relative;
+  right: -6%;
+}
+
+img {
+  display: block;
+  width: auto;
+  max-height: clamp(200px, 40vw, 600px);
+}
+
+.first-image img {
+  transform: translateY(30%);
+}
+</style>
